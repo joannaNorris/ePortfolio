83da04ae7c7bedc7234a380a9d3519a85d8cfa19
joannaNorris <168397104+joannaNorris@users.noreply.github.com>
2026-08-01 12:35:09 -0500

    Delete app_server directory
    using a different project

--- app_server/routes/contact.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/contact');
-
-router.get('/', controller.contact);
-router.post('/', controller.contactSubmit);
-
-module.exports = router;
--- app_server/routes/meals.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var router = express.Router();
-const controller = require('../controllers/meals');
-
-router.get('/', controller.meals);
-
-module.exports = router;
